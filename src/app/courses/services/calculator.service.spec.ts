4713cb53ab6d14798f0aa8133174dd731a74253d
Dagony <dagony@gmail.com>
2020-12-17 11:54:29 +0100

    5 - quick introduction to jasmine test suites and specifications

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,12 @@
+import {CalculatorService} from './calculator.service';
+
+describe('CalculatorService', () => {
+  it('should add two numbers', () => {
+    pending();
+    // const cs = new CalculatorService();
+    // expect(CalculatorService.add(1, 2)).toEqual(3);
+  });
+  it('should subtract two numbers', () => {
+    fail();
+  });
+});
